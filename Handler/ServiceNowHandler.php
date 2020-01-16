9f65b5b70e27e939f1f76a23f5cf4a6c81567bc5
Jérôme Coppée <j.coppee@gmail.com>
2020-01-16 17:05:40 +0100

    feature (serviceNowHandler) : add attachment support (#18)
    * feature (serviceNowHandler) : add attachment support
    
    * fix : phpcs
    
    * refactor : phpcs
    
    * refactor : phpstan
    
    * refactor : @dameert review

--- Handler/ServiceNowHandler.php
+++ Handler/ServiceNowHandler.php
@@ -12,6 +12,7 @@ use EMS\SubmissionBundle\Service\SubmissionRenderer;
 use EMS\SubmissionBundle\Submit\ServiceNowResponse;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class ServiceNowHandler extends AbstractHandler
 {
@@ -19,11 +20,14 @@ class ServiceNowHandler extends AbstractHandler
     private $renderer;
     /** @var int */
     private $timeout;
+    /** @var HttpClientInterface */
+    private $client;
 
     public function __construct(SubmissionRenderer $renderer, int $timeout)
     {
         $this->renderer = $renderer;
         $this->timeout = $timeout;
+        $this->client = HttpClient::create();
     }
 
     public function handle(SubmissionConfig $submission, FormInterface $form, FormConfig $config, AbstractResponse $previousResponse = null): AbstractResponse
@@ -32,20 +36,71 @@ class ServiceNowHandler extends AbstractHandler
             $renderedSubmission = $this->renderer->render($submission, $form, $config, $previousResponse);
             $snow = new ServiceNowConfig($renderedSubmission);
 
-            $client = HttpClient::create();
-            $response = $client->request('POST', $snow->getHost(), [
+            $response = $this->client->request('POST', $snow->getBodyEndpoint(), [
                 'headers' => [
                     'Accept' => 'application/json',
                     'Content-Type' => 'application/json',
                     'Authorization' => $snow->getBasicAuth(),
                 ],
-                'body' => $snow->getFieldsJson(),
+                'body' => $snow->getBody(),
                 'timeout' => $this->timeout
             ]);
 
-            return new ServiceNowResponse($response->getContent());
+            $serviceNowResponse = new ServiceNowResponse($response->getContent());
+            $this->addAttachments($serviceNowResponse, $snow);
+
+            return $serviceNowResponse;
         } catch (\Exception $exception) {
-            return new FailedResponse(sprintf('Submission failed, contact your admin. %s', $exception->getMessage()));
+            return new FailedResponse(\sprintf('Submission failed, contact your admin. %s', $exception->getMessage()));
+        }
+    }
+
+    private function addAttachments(ServiceNowResponse $response, ServiceNowConfig $config): void
+    {
+        foreach ($config->getAttachments() as $attachment) {
+            $binary = $this->getBinaryFile($attachment['pathname']);
+
+            if (!empty($binary)) {
+                $this->postAttachment($response, $config, $attachment, $binary);
+            }
+        }
+    }
+
+    private function getBinaryFile(string $pathname): ?string
+    {
+        $file = \fopen($pathname, "r");
+        $size = \filesize($pathname);
+
+        if (!$file || !$size) {
+            return null;
+        }
+
+        $binary = \fread($file, $size);
+
+        if (!$binary) {
+            return null;
+        }
+
+        return $binary;
+    }
+
+    private function postAttachment(ServiceNowResponse $response, ServiceNowConfig $config, array $attachment, string $binary)
+    {
+        try {
+            $this->client->request('POST', $config->getAttachmentEndpoint(), [
+                'query' => [
+                    'file_name' => $attachment['originalName'],
+                    'table_name' => $config->getTable(),
+                    'table_sys_id' => $response->getResultProperty('sys_id'),
+                ],
+                'headers' => [
+                    'Content-Type' => $attachment['mimeType'],
+                    'Authorization' => $config->getBasicAuth(),
+                ],
+                'body' => $binary
+            ]);
+        } catch (\Exception $exception) {
+            return new FailedResponse(\sprintf('Attachment submission failed, contact your admin. %s', $exception->getMessage()));
         }
     }
 }
