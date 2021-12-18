64494dc502f5ed27d536b46efbe91d099a686477
Mathieu De Keyzer <me@theus.be>
2021-12-18 09:30:06 +0100

    feat: align with core (#85)
    * ref: align with core entities
    
    * chore: update phpcs
    
    * chore: update github workflows
    
    * refactor: doc
    
    * refactor: doc
    
    * refactor: doc
    
    * feat: add all setters and getters

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -5,17 +5,19 @@ if (!file_exists(__DIR__.'/src')) {
 }
 
 $finder = PhpCsFixer\Finder::create()
-    ->in(__DIR__)
-    ->exclude('vendor')
+    ->in(__DIR__.'/src')
+    ->in(__DIR__.'/tests')
     ->exclude('tests/tmp')
 ;
 
-return PhpCsFixer\Config::create()
+$config = new PhpCsFixer\Config();
+
+return $config
     ->setRules([
         '@Symfony' => true,
         'declare_strict_types' => true,
         'final_class' => true,
-        'native_function_invocation' => true,
+        'native_function_invocation' => ['include' => ['@all']],
         'no_unused_imports' => true
     ])
     ->setRiskyAllowed(true)
--- src/Entity/FormSubmissionFile.php
+++ src/Entity/FormSubmissionFile.php
@@ -49,7 +49,7 @@ class FormSubmissionFile
     private $formSubmission;
 
     /**
-     * @var string
+     * @var string|resource
      *
      * @ORM\Column(name="file", type="blob")
      */
@@ -107,5 +107,56 @@ class FormSubmissionFile
     public function updateModified(): void
     {
         $this->modified = new \DateTime();
+    }
+
+    /**
+     * @return resource|null
+     */
+    public function getFile()
+    {
+        return \is_resource($this->file) ? $this->file : null;
+    }
+
+    public function getFilename(): string
+    {
+        return $this->filename;
+    }
+
+    public function getId(): UuidInterface
+    {
+        return $this->id;
+    }
+
+    public function getCreated(): \DateTime
+    {
+        return $this->created;
+    }
+
+    public function getModified(): \DateTime
+    {
+        return $this->modified;
+    }
+
+    public function getFormSubmission(): FormSubmission
+    {
+        return $this->formSubmission;
+    }
+
+    public function getFormField(): string
+    {
+        return $this->formField;
+    }
+
+    public function getMimeType(): string
+    {
+        return $this->mimeType;
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function getSize()
+    {
+        return $this->size;
     }
 }
